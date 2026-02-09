fee5d61e2af9848708adfc2614f83a3c4dea67f6
RutujaBhosle <bhoslerutujar@gmail.com>
2026-02-10 01:41:44 +0530

    fix styling issues

--- controllers/listings.js
+++ controllers/listings.js
@@ -65,7 +65,7 @@ module.exports.renderEditForm = async (req, res) => {
         res.redirect("/listings");
     };
     let originalImageUrl = listing.image.url;
-    originalImageUrl = originalImageUrl.replace("/upload", "upload/w_250");
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
     res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
